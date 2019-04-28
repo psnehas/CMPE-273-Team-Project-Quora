5f34fadf49c0e759428901e1ed16bad84bfd761f
chuang5 <chen-feng.huang@sjsu.edu>
2019-04-28 00:30:54 -0700

    complete answer service

--- kakfa/server/services/answer.js
+++ kakfa/server/services/answer.js
@@ -3,54 +3,59 @@ var formidable = require('formidable')
 var fs = require('fs')
 var path = require('path')
 
-const upvote = (req, res) => {
-    db.upvoteAnswer(req.body.answer_id).then(() =>{
+const upvote = (req, next) => {
+    db.upvoteAnswer(req.body.answer_id).then(result =>{
         console.log("Upvote answer ", req.body.answer_id)
-        res.status(200).json({
-            success: "Upvote answer "+ req.body.answer_id
+        next(null, {
+            status: 200,
+            data: result.messages
         })
     })
 }
 
-const downvote = (req, res) => {
-    db.downvoteAnswer(req.body.answer_id).then(() =>{
+const downvote = (req, next) => {
+    db.downvoteAnswer(req.body.answer_id).then(result =>{
         console.log("Downvote answer ", req.body.answer_id)
-        res.status(200).json({
-            success: "Downvote answer "+ req.body.answer_id
+        next(null, {
+            status: 200,
+            data: result.messages
         })
     })
 }
 
-const allVotes = (req, res) => {
+const allVotes = (req, next) => {
     db.getVotes(req.params.answer_id).then(result =>{
         console.log("Upvotes: ", result.upvote)
         console.log("Downvotes: ", result.downvote)
-        res.status(200).json({
-            allVotes: result
+        next(null, {
+            status: 200,
+            data: result.messages
         })
     })
 }
 
-const allComments = (req, res) => {
+const allComments = (req, next) => {
     db.getComments(req.params.answer_id).then(result =>{
         console.log("allComments: ", result.comments)
-        res.status(200).json({
-            success: result.comments
+        next(null, {
+            status: 200,
+            data: result.messages
         })
     })
 }
 
-const makeComment = (req, res) => {
+const makeComment = (req, next) => {
     console.log("Answer ID: ", req.body.answer_id)
     console.log("Comment: ", req.body.comment)
     db.createComment(req.body.answer_id, req.body.comment).then(() =>{
-        res.status(200).json({
-            success: 'Comment created'
+        next(null, {
+            status: 200,
+            data: result.messages
         })
     })
 }
 
-const makeAnswer = (req, res) => {
+const makeAnswer = (req, next) => {
     console.log("question ID: ", req.params.question)
     console.log("Comment: ", req.body.comment)
     newAnswer ={
@@ -63,13 +68,14 @@ const makeAnswer = (req, res) => {
         comments: [],
     }
     db.createAnswer(newAnswer).then(() =>{
-        res.status(200).json({
-            success: 'Answer created'
+        next(null, {
+            status: 200,
+            data: result.messages
         })
     })
 }
 
-const updateAnswer = (req, res) => {
+const updateAnswer = (req, next) => {
     console.log("question ID: ", req.params.question)
     console.log("Comment: ", req.body.comment)
     editInfo ={
@@ -79,29 +85,65 @@ const updateAnswer = (req, res) => {
         content: req.body.answer,
     }
     db.updateOneAnswer(editInfo).then(() =>{
-        res.status(200).json({
-            success: 'Answer Edited'
+        next(null, {
+            status: 200,
+            data: result.messages
         })
     })
 }
 
-const createBookmark = (req, res) => {
+const createBookmark = (req, next) => {
     console.log("Bookmark added")
     db.setBookmark(req.body.user_id, req.body.answer_id).then(() =>{
-        res.status(200).json({
-            success: 'Bookmark added'
+        next(null, {
+            status: 200,
+            data: result.messages
         })
     })
 }
 
-const getOneAnswer = (req, res) => {
+const getOneAnswer = (req, next) => {
     db.findOneAnswer(req.params.answer_id).then(result =>{
         console.log("Answer content: ", result)
-        res.status(200).json({
-            answerContent: result
+        next(null, {
+            status: 200,
+            data: result.messages
         })
     })
 }
 
-module.exports = {upvote, downvote, allVotes, allComments, makeComment, createBookmark,
-    getOneAnswer, makeAnswer, updateAnswer}
+const dispatch = (message, next) => {
+    switch (message.cmd) {
+        case 'UPVOTE':
+            upvote(message.req, next);
+            break;
+        case 'DOWNVOTE':
+            downvote(message.req, next);
+            break;
+        case 'ALLVOTES':
+            allVotes(message.req, next);
+            break;
+        case 'ALLCOMMENTS':
+            allComments(message.req, next);
+            break;
+        case 'MAKECOMMENT':
+            makeComment(message.req, next);
+            break;
+        case 'MAKEANSWER':
+            makeAnswer(message.req, next);
+            break;
+        case 'UPDATEANSWER':
+            updateAnswer(message.req, next);
+            break;
+        case 'CREATEBOOKMARK':
+            createBookmark(message.req, next);
+            break;
+        case 'GETONEANSWER':
+            getOneAnswer(message.req, next);
+            break;
+        default:
+            console.log('unknown request');
+    }
+}
+
+module.exports = {dispatch}
