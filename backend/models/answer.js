31aebf8bd3f715f9dbc304d50426a0f802784edb
chuang5 <chen-feng.huang@sjsu.edu>
2019-05-01 01:51:39 -0700

    move comment schema ahead to answer schema

--- backend/models/answer.js
+++ backend/models/answer.js
@@ -1,6 +1,18 @@
 var mongoose = require('mongoose')
 var autoIncrement = require('mongoose-sequence')(mongoose)
 
+const CommentSchema = new mongoose.Schema({
+    owner: {
+        type: Number // user_id
+    },
+    time: {
+        type: String
+    },
+    comment: {
+        type: String
+    }
+})
+
 const AnswerSchema = new mongoose.Schema({
     question_id: {
         type: Number,
@@ -29,19 +41,8 @@ const AnswerSchema = new mongoose.Schema({
     comments: [CommentSchema]
 })
 
-const CommentSchema = new mongoose.Schema({
-    owner: {
-        type: Number // user_id
-    },
-    time: {
-        type: String
-    },
-    comment: {
-        type: String
-    }
-})
 
-AnswerSchema.plugin(autoIncrement, {inc_field: 'answer_id'});
 CommentSchema.plugin(autoIncrement, {inc_field: 'comment_id'});
+AnswerSchema.plugin(autoIncrement, {inc_field: 'answer_id'});
 
 module.exports =  mongoose.model('Answer', AnswerSchema);
