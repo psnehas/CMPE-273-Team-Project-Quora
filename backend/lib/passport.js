32b94acc93b8688375c0e7d59c736ba7500079c5
ethandu7 <ethandu7@ethandu7s-MacBook-Pro.local>
2019-04-30 23:11:22 -0700

    disable redis for loacl testing

--- backend/lib/passport.js
+++ backend/lib/passport.js
@@ -2,13 +2,14 @@ var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
 var User = require('../models/user');
 var passport = require('passport');
-var redis = require('./redis');
+//var redis = require('./redis');
 
 passport.use(new JwtStrategy({
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     secretOrKey: 'quora_secret_key'
 }, function(jwt_payload, done) {
     console.log('In passport verification, the jwt_payload is: ', jwt_payload);
+    /*
     redis.get(jwt_payload.email)
     .then(result => {
         if (!result) {
@@ -32,6 +33,18 @@ passport.use(new JwtStrategy({
             return done(null, JSON.parse(result));
         }
     })
+    */
+    User.findOne({email: jwt_payload.email}, function(err, user) {
+        console.log('find user result: ', user);
+        if (err) {
+            return done(err, false);
+        }
+        if (user) {
+            return done(null, jwt_payload);
+        } else {
+            return done(null, false);
+        }
+    });
 }));
 
 const requireSigninPassport = passport.authenticate('jwt', {session: false});
