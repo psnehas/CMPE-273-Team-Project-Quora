3cf33af7748068983c604e837579fc8174325aba
chuang5 <chen-feng.huang@sjsu.edu>
2019-05-03 00:23:30 -0700

    edit owner type from number to string (email), and fix minor bug

--- backend/lib/mongoDB.js
+++ backend/lib/mongoDB.js
@@ -106,7 +106,7 @@ exports.getComments = (answerid) => {
 }
 
 exports.createComment = (comment) => {
-    return Answer.findOneAndUpdate({answer_id: comment.answer_id}, {$push: {comments: {owner: comment.owner, time : comment.time, comment : comment.content}}}).exec();
+    return Answer.findOneAndUpdate({answer_id: comment.answer_id}, {$push: {comments: {owner: comment.owner, time : comment.time, comment : comment.comment}}}).exec();
 }
 
 exports.createAnswer = (data) => {
