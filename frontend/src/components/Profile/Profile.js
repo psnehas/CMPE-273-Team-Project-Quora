790276f87dbc9510c42e5194918cff4b7f12b671
Yuxiang Chen <yuxiang.chen@sjsu.edu>
2019-05-04 01:18:35 -0700

    update profile info

--- frontend/src/components/Profile/Profile.js
+++ frontend/src/components/Profile/Profile.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
 import style from './Profile.module.css';
-import { Container, Col, Row, Image, Modal, Button } from 'react-bootstrap';
+import { Container, Col, Row, Image, Modal, Button, Form } from 'react-bootstrap';
 import moment from 'moment';
 import Content from './Content/Content'
 import Sidebar from './Sidebar/Sidebar';
 
+import axios from 'axios';
+
 class Profile extends Component {
 
     state = {
+        // Data
         user_info: {},
         created_answers: [],
         bookmarked_answers: [],
         created_questions: [],
         followed_people: [],
         following_people: [],
+        // Upload New Avatar
         show_edit_image: false,
-        show_image_uploader: false
+        show_image_uploader: false,
+        selectedFile: null,
+        loaded: 0,
+        // Update Profile Info
+        editing_profile: false,
+        new_first_name: "",
+        new_first_name: "",
+        new_last_name: "",
+        new_profileCredential: "",
+        new_about: ""
     };
 
     componentDidMount() {
@@ -93,12 +106,76 @@ class Profile extends Component {
         })
     }
 
-    onCloseImageUploaderHandler = () =>{
+    onCloseImageUploaderHandler = () => {
         this.setState({
             show_image_uploader: false
         })
     }
 
+    handleselectedFile = event => {
+        this.setState({
+            selectedFile: event.target.files[0],
+            loaded: 0,
+        })
+    }
+
+    handleUpload = () => {
+        if (!this.state.selectedFile) return;
+        const data = new FormData()
+        data.append('avatar', this.state.selectedFile, this.state.selectedFile.name)
+        console.log(data);
+        // TODO
+        // axios
+        //     .post(config.host + '/profile/update_avatar', data, {
+        //         onUploadProgress: ProgressEvent => {
+        //             this.setState({
+        //                 loaded: (ProgressEvent.loaded / ProgressEvent.total * 100),
+        //             })
+        //         },
+        //     })
+        //     .then(res => {
+        //         console.log(res.statusText)
+        //         this.loadCourseFiles()
+        //     })
+    }
+
+    onEditProfileHandler = () => {
+        this.setState({
+            editing_profile: true,
+            new_first_name: this.state.user_info.first_name,
+            new_last_name: this.state.user_info.last_name,
+            new_profileCredential: this.state.user_info.profileCredential,
+            new_about: this.state.user_info.about
+        })
+    }
+
+    onCancelEditProfileHandler = () => {
+        this.setState({
+            editing_profile: false
+        })
+    }
+
+    onSaveEditProfileHandler = () => {
+
+        let new_user_info = {
+            avatar: this.state.user_info.avatar,
+            first_name: this.state.new_first_name,
+            last_name: this.state.last_name,
+            profileCredential: this.state.new_profileCredential,
+            about: this.state.new_about,
+        }
+
+        this.setState({
+            editing_profile: false,
+            user_info: new_user_info
+        })
+
+        // TODO
+    }
+
+    onChangeHandler = (e) => {
+        this.setState({ [e.target.id]: e.target.value });
+    };
 
     render() {
 
@@ -109,6 +186,44 @@ class Profile extends Component {
             followed_people: this.state.followed_people,
             following_people: this.state.following_people
         }
+
+        let profile_content = (
+            <div>
+                <h1>{this.state.user_info.first_name}&nbsp;{this.state.user_info.last_name}</h1>
+                <p>{this.state.user_info.profileCredential}</p>
+                <p>{this.state.user_info.about}</p>
+                <Button onClick={this.onEditProfileHandler}>Edit</Button>
+            </div>
+        )
+
+        let profile_edit = (
+            <div>
+                <Form>
+                    <Form.Row>
+                        <Form.Group as={Col} controlId="new_first_name">
+                            <Form.Label>First Name</Form.Label>
+                            <Form.Control type="text" placeholder="First Name" value={this.state.new_first_name} onChange={this.onChangeHandler} />
+                        </Form.Group>
+                        <Form.Group as={Col} controlId="new_last_name">
+                            <Form.Label>Last Name</Form.Label>
+                            <Form.Control type="text" placeholder="Last Name" value={this.state.new_last_name} onChange={this.onChangeHandler} />
+                        </Form.Group>
+                    </Form.Row>
+                    <Form.Group controlId="new_profileCredential">
+                        <Form.Label>Profile Credential</Form.Label>
+                        <Form.Control type="text" placeholder="Profile Credential" value={this.state.new_profileCredential} onChange={this.onChangeHandler} />
+                    </Form.Group>
+                    <Form.Group controlId="new_about">
+                        <Form.Label>About</Form.Label>
+                        <Form.Control type="text" placeholder="About" value={this.state.new_about} onChange={this.onChangeHandler} />
+                    </Form.Group>
+
+                    <Button onClick={this.onCancelEditProfileHandler} className={style.marginRight}>Cancel</Button>
+                    <Button onClick={this.onSaveEditProfileHandler}>Save</Button>
+
+                </Form>
+            </div>
+        )
 
         return (
             <React.Fragment>
@@ -121,10 +236,13 @@ class Profile extends Component {
                             </div>
                         </Col>
                         <Col xs={8}>
-                            <h1>{this.state.user_info.first_name}&nbsp;{this.state.user_info.last_name}</h1>
+                            {this.state.editing_profile ? profile_edit : profile_content}
                         </Col>
                     </Row>
                     <hr />
+
+
+
                     <Row>
                         <Col>
                             <Sidebar uid={this.state.user_info.uid} />
@@ -133,16 +251,22 @@ class Profile extends Component {
                             <Content uid={this.state.user_info.uid} data={context} />
                         </Col>
                     </Row>
+
+
                     <Modal show={this.state.show_image_uploader} onHide={this.onCloseImageUploaderHandler}>
                         <Modal.Header closeButton>
                             <Modal.Title>Modal heading</Modal.Title>
                         </Modal.Header>
-                        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                        <Modal.Body>
+                            <Form.Control type="file" onChange={this.handleselectedFile} />
+                            <br />
+                            <div> {Math.round(this.state.loaded, 2)} %</div>
+                        </Modal.Body>
                         <Modal.Footer>
                             <Button variant="secondary" onClick={this.onCloseImageUploaderHandler}>
                                 Close
                             </Button>
-                            <Button variant="primary" onClick={this.onCloseImageUploaderHandler}>
+                            <Button variant="primary" onClick={this.handleUpload}>
                                 Save Changes
                             </Button>
                         </Modal.Footer>
