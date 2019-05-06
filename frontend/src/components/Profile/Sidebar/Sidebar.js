165dfa7e3e2e28f0d284152d8a2e8e090c139c93
Yuxiang Chen <yuxiang.chen@sjsu.edu>
2019-05-06 02:49:50 -0700

    update sidebar

--- frontend/src/components/Profile/Sidebar/Sidebar.js
+++ frontend/src/components/Profile/Sidebar/Sidebar.js
@@ -1,36 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Nav } from 'react-bootstrap';
-import { withRouter, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { userActions } from '../../../_actions';
 
 import style from '../Profile.module.css';
 
-const Sidebar = (props) => {
-    console.log("Profile Sidebar uid", props.uid);
-    return (
-        <Nav className="flex-column" variant="pills">
-            <h3 className={style.contentTitle}>
-                <div>
-                    Feeds
+class Sidebar extends Component {
+
+    render(){
+        console.log("Profile Sidebar uid", this.props.uid);
+        return (
+            <Nav className="flex-column" variant="pills">
+                <h3 className={style.contentTitle}>
+                    <div>
+                        Feeds
                 </div>
-            </h3>
-            <NavLink to={`/profile/${props.uid}`} className="nav-link" exact>
-                Profile
+                </h3>
+                <NavLink to={`/profile/${this.props.authentication.user_id}`} className="nav-link" exact>
+                    Profile
             </NavLink>
-            <NavLink to={`/profile/${props.uid}/answers`} className="nav-link">
-                Answers
+                <NavLink to={`/profile/${this.props.authentication.user_id}/answers`} className="nav-link">
+                    Answers
             </NavLink>
-            <NavLink to={`/profile/${props.uid}/questions`} className="nav-link">
-                Questions
+                <NavLink to={`/profile/${this.props.authentication.user_id}/questions`} className="nav-link">
+                    Questions
             </NavLink>
-            <NavLink to={`/profile/${props.uid}/followers`} className="nav-link">
-                Followers
+                <NavLink to={`/profile/${this.props.authentication.user_id}/followers`} className="nav-link">
+                    Followers
             </NavLink>
-            <NavLink to={`/profile/${props.uid}/following`} className="nav-link">
-                Following
+                <NavLink to={`/profile/${this.props.authentication.user_id}/following`} className="nav-link">
+                    Following
             </NavLink>
 
-        </Nav>
-    )
+            </Nav>
+        )
+    }
+
 }
 
-export default withRouter(Sidebar);
+
+// reducer: profile 's output maps to this.props.profile
+const mapStateToProps = ({ authentication }) => ({ authentication });
+// apply above mapping to Login class
+export default connect(mapStateToProps)(Sidebar);
