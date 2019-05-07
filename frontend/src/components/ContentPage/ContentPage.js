5c922aa1afaf174ef22880a8d05d0158c87281ec
Yuxiang Chen <yuxiang.chen@sjsu.edu>
2019-05-06 23:53:48 -0700

    deactive, follower

---
+++ frontend/src/components/ContentPage/ContentPage.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Container, Col, Row, Image, Modal, Button, Form } from 'react-bootstrap';
+
+class ContentPage extends Component {
+
+    render() {
+        return (
+            <React.Fragment>
+                <Container>
+                    <h3>Content Page</h3>
+                </Container>
+            </React.Fragment>
+        )
+    }
+}
+
+export default ContentPage;
--- frontend/src/components/Profile/Content/FollowingContent/FollowingContent.js
+++ frontend/src/components/Profile/Content/FollowingContent/FollowingContent.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Nav, ListGroup } from 'react-bootstrap';
+import { connect } from 'react-redux';
 
 import style from '../../Profile.module.css'
 
 class FollowingContent extends Component {
     render() {
+
+        console.log("following", this.props.profile)
         return (
-            <h3 className={style.contentTitle}>
-                <div>
-                    Following
-                </div>
-            </h3>
+            <React.Fragment>
+                <h3 className={style.contentTitle}>
+                    Followings
+                </h3>
+                <ListGroup>
+                    {this.props.profile.followed_people.map((p, index) => {
+                        return (
+                            <ListGroup.Item key={p._id}>
+                                <Link to={`/profile/${p._id}`}>{`${p.first_name} ${p.last_name}`}</Link>
+                            </ListGroup.Item>
+                        )
+                    })}
+                </ListGroup>
+            </React.Fragment>
         )
     }
 }
 
-export default FollowingContent;
+// reducer: profile 's output maps to this.props.profile
+const mapStateToProps = ({ profile }) => ({ profile });
+// apply above mapping to Login class
+export default connect(mapStateToProps)(FollowingContent);
