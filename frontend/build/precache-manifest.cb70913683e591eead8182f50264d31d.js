5489bf40af22eea8d3ffb07321d3192b9c10fc4e
Yu Zhao <yu.zhao@sjsu.edu>
2019-05-07 15:16:26 -0700

    build

---
+++ frontend/build/precache-manifest.cb70913683e591eead8182f50264d31d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "82a73a301818968e11f6",
+    "url": "/static/js/main.82a52589.chunk.js"
+  },
+  {
+    "revision": "03d1a0c90a6aa284b418",
+    "url": "/static/js/2.1031e84d.chunk.js"
+  },
+  {
+    "revision": "82a73a301818968e11f6",
+    "url": "/static/css/main.e4043f88.chunk.css"
+  },
+  {
+    "revision": "f424dde71caa8abd4ec2c467d3407b0c",
+    "url": "/index.html"
+  }
+];
