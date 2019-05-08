dcda006365f4ae65c629207c3c768fe7d28a8fab
chuang5 <chen-feng.huang@sjsu.edu>
2019-05-07 21:43:23 -0700

    mocha test token changed

--- backend/test.js
+++ backend/test.js
@@ -12,8 +12,8 @@ describe('Quora simulation', function(){
     it('POST /signin',function(){
         agent.post('/signin')
             .send({
-                email: "test40000@sjsu.com",
-                password: "123456"
+                email: "john.smith@sjsu.edu",
+                password: "gugu"
             })
             .end(function(err, res) {
                 console.log("Mocha test -- signin:")
@@ -23,7 +23,7 @@ describe('Quora simulation', function(){
     
     it('PUT /answer/5ccfa29a6df7dc08985b1580/upvote',function(){
         agent.put('/answer/5ccfa29a6df7dc08985b1580/upvote')
-            .set({'Authorization':'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3Q0MDAwMEBzanN1LmNvbSIsInVzZXJfaWQiOiI1Y2NmYTEyY2ZjOTk3Njc3ZjFhYTA4YmQiLCJpYXQiOjE1NTcxMzIyODB9.XfPhLw8THWftJvr4iiEvf2guuTTH2Yv2vF-QvHUUFSA'})
+            .set({'Authorization':'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImpvaG4uc21pdGhAc2pzdS5lZHUiLCJ1c2VyX2lkIjoiNWNkMWQ3YzIxNGY3NmUzNmVkYTYzZjBkIiwiaWF0IjoxNTU3Mjc0NDIwfQ.ItJ0CmHusYy7vkegfP0EuNkqI0cbSgpzBdCpHRxr3mM'})
             .end(function(err, res) {
                 console.log("Mocha test -- upvote:")
                 console.log(res.body)
@@ -32,7 +32,7 @@ describe('Quora simulation', function(){
     
     it('GET /search/user/test40000',function(){
         agent.get('/search/user/test40000')
-            .set({'Authorization':'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3Q0MDAwMEBzanN1LmNvbSIsInVzZXJfaWQiOiI1Y2NmYTEyY2ZjOTk3Njc3ZjFhYTA4YmQiLCJpYXQiOjE1NTcxMzIyODB9.XfPhLw8THWftJvr4iiEvf2guuTTH2Yv2vF-QvHUUFSA'})
+            .set({'Authorization':'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImpvaG4uc21pdGhAc2pzdS5lZHUiLCJ1c2VyX2lkIjoiNWNkMWQ3YzIxNGY3NmUzNmVkYTYzZjBkIiwiaWF0IjoxNTU3Mjc0NDIwfQ.ItJ0CmHusYy7vkegfP0EuNkqI0cbSgpzBdCpHRxr3mM'})
             .end(function(err, res) {
                 console.log("Mocha test -- search user:")
                 console.log(res.body)
@@ -41,7 +41,7 @@ describe('Quora simulation', function(){
 
     it('GET /search/question/question',function(){
         agent.get('/search/question/question')
-            .set({'Authorization':'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3Q0MDAwMEBzanN1LmNvbSIsInVzZXJfaWQiOiI1Y2NmYTEyY2ZjOTk3Njc3ZjFhYTA4YmQiLCJpYXQiOjE1NTcxMzIyODB9.XfPhLw8THWftJvr4iiEvf2guuTTH2Yv2vF-QvHUUFSA'})
+            .set({'Authorization':'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImpvaG4uc21pdGhAc2pzdS5lZHUiLCJ1c2VyX2lkIjoiNWNkMWQ3YzIxNGY3NmUzNmVkYTYzZjBkIiwiaWF0IjoxNTU3Mjc0NDIwfQ.ItJ0CmHusYy7vkegfP0EuNkqI0cbSgpzBdCpHRxr3mM'})
             .end(function(err, res) {
                 console.log("Mocha test -- search question:")
                 console.log(res.body)
@@ -50,7 +50,7 @@ describe('Quora simulation', function(){
 
     it('GET /search/topic/08',function(){
         agent.get('/search/topic/08')
-            .set({'Authorization':'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3Q0MDAwMEBzanN1LmNvbSIsInVzZXJfaWQiOiI1Y2NmYTEyY2ZjOTk3Njc3ZjFhYTA4YmQiLCJpYXQiOjE1NTcxMzIyODB9.XfPhLw8THWftJvr4iiEvf2guuTTH2Yv2vF-QvHUUFSA'})
+            .set({'Authorization':'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImpvaG4uc21pdGhAc2pzdS5lZHUiLCJ1c2VyX2lkIjoiNWNkMWQ3YzIxNGY3NmUzNmVkYTYzZjBkIiwiaWF0IjoxNTU3Mjc0NDIwfQ.ItJ0CmHusYy7vkegfP0EuNkqI0cbSgpzBdCpHRxr3mM'})
             .end(function(err, res) {
                 console.log("Mocha test -- search topic:")
                 console.log(res.body)
@@ -59,7 +59,7 @@ describe('Quora simulation', function(){
 
     it('GET /questions/5cd0b395a2bcc65d0481ddee',function(){
         agent.get('/questions/5cd0b395a2bcc65d0481ddee')
-            .set({'Authorization':'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3Q0MDAwMEBzanN1LmNvbSIsInVzZXJfaWQiOiI1Y2NmYTEyY2ZjOTk3Njc3ZjFhYTA4YmQiLCJpYXQiOjE1NTcxMzIyODB9.XfPhLw8THWftJvr4iiEvf2guuTTH2Yv2vF-QvHUUFSA'})
+            .set({'Authorization':'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImpvaG4uc21pdGhAc2pzdS5lZHUiLCJ1c2VyX2lkIjoiNWNkMWQ3YzIxNGY3NmUzNmVkYTYzZjBkIiwiaWF0IjoxNTU3Mjc0NDIwfQ.ItJ0CmHusYy7vkegfP0EuNkqI0cbSgpzBdCpHRxr3mM'})
             .end(function(err, res) {
                 console.log("Mocha test -- fetch question:")
                 console.log(res.body)
@@ -68,7 +68,7 @@ describe('Quora simulation', function(){
 
     it('GET /userTopics',function(){
         agent.get('/userTopics')
-            .set({'Authorization':'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3Q0MDAwMEBzanN1LmNvbSIsInVzZXJfaWQiOiI1Y2NmYTEyY2ZjOTk3Njc3ZjFhYTA4YmQiLCJpYXQiOjE1NTcxMzIyODB9.XfPhLw8THWftJvr4iiEvf2guuTTH2Yv2vF-QvHUUFSA'})
+            .set({'Authorization':'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImpvaG4uc21pdGhAc2pzdS5lZHUiLCJ1c2VyX2lkIjoiNWNkMWQ3YzIxNGY3NmUzNmVkYTYzZjBkIiwiaWF0IjoxNTU3Mjc0NDIwfQ.ItJ0CmHusYy7vkegfP0EuNkqI0cbSgpzBdCpHRxr3mM'})
             .end(function(err, res) {
                 console.log("Mocha test -- get user topics:")
                 console.log(res.body)
@@ -77,7 +77,7 @@ describe('Quora simulation', function(){
 
     it('GET /userFeed',function(){
         agent.get('/userFeed')
-            .set({'Authorization':'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3Q0MDAwMEBzanN1LmNvbSIsInVzZXJfaWQiOiI1Y2NmYTEyY2ZjOTk3Njc3ZjFhYTA4YmQiLCJpYXQiOjE1NTcxMzIyODB9.XfPhLw8THWftJvr4iiEvf2guuTTH2Yv2vF-QvHUUFSA'})
+            .set({'Authorization':'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImpvaG4uc21pdGhAc2pzdS5lZHUiLCJ1c2VyX2lkIjoiNWNkMWQ3YzIxNGY3NmUzNmVkYTYzZjBkIiwiaWF0IjoxNTU3Mjc0NDIwfQ.ItJ0CmHusYy7vkegfP0EuNkqI0cbSgpzBdCpHRxr3mM'})
             .end(function(err, res) {
                 console.log("Mocha test -- get user feed:")
                 console.log(res.body)
@@ -86,7 +86,7 @@ describe('Quora simulation', function(){
 
     it('GET /profile/5ccfa12cfc997677f1aa08bd',function(){
         agent.get('/profile/5ccfa12cfc997677f1aa08bd')
-            .set({'Authorization':'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3Q0MDAwMEBzanN1LmNvbSIsInVzZXJfaWQiOiI1Y2NmYTEyY2ZjOTk3Njc3ZjFhYTA4YmQiLCJpYXQiOjE1NTcxMzIyODB9.XfPhLw8THWftJvr4iiEvf2guuTTH2Yv2vF-QvHUUFSA'})
+            .set({'Authorization':'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImpvaG4uc21pdGhAc2pzdS5lZHUiLCJ1c2VyX2lkIjoiNWNkMWQ3YzIxNGY3NmUzNmVkYTYzZjBkIiwiaWF0IjoxNTU3Mjc0NDIwfQ.ItJ0CmHusYy7vkegfP0EuNkqI0cbSgpzBdCpHRxr3mM'})
             .end(function(err, res) {
                 console.log("Mocha test -- get profile:")
                 console.log(res.body)
@@ -95,7 +95,7 @@ describe('Quora simulation', function(){
 
     it('GET /answer/5ccfe10025f0565300627180/owner',function(){
         agent.get('/answer/5ccfe10025f0565300627180/owner')
-            .set({'Authorization':'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3Q0MDAwMEBzanN1LmNvbSIsInVzZXJfaWQiOiI1Y2NmYTEyY2ZjOTk3Njc3ZjFhYTA4YmQiLCJpYXQiOjE1NTcxMzIyODB9.XfPhLw8THWftJvr4iiEvf2guuTTH2Yv2vF-QvHUUFSA'})
+            .set({'Authorization':'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImpvaG4uc21pdGhAc2pzdS5lZHUiLCJ1c2VyX2lkIjoiNWNkMWQ3YzIxNGY3NmUzNmVkYTYzZjBkIiwiaWF0IjoxNTU3Mjc0NDIwfQ.ItJ0CmHusYy7vkegfP0EuNkqI0cbSgpzBdCpHRxr3mM'})
             .end(function(err, res) {
                 console.log("Mocha test -- get answer's owner:")
                 console.log(res.body)
